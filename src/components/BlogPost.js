5f9cdcc890698f5ef9dd512a73cfa168ace5a594
Morgan Polak <polakmaor@gmail.com>
2023-06-24 00:46:46 +0100

    ENH: makes the date field for blog posts responsive

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -10,11 +10,11 @@ export default function BlogPost({
     const renderMetadata = () => {
         return (
             <div className="top" data-anchor={title}>
-                <div className="post-title">
-                    {title}
-                </div>
                 <div className="date">
                     {createdAt}
+                </div>
+                <div className="post-title">
+                    {title}
                 </div>
             </div>
         );
