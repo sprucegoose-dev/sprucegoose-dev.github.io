85c0dfa067457b7929480b1955c342e635a8797b
Morgan Polak <polakmaor@gmail.com>
2026-08-30 03:30:42 +0100

    removes a link from the navigation menu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { useNavigate } from 'react-router-dom';
 
@@ -39,7 +39,6 @@ export default function MainMenu() {
             <span className="menu-item" data-target="#education" onClick={(event) => handleMenuClick(event)}>Education</span>
             <span className="menu-item" data-target="#projects" onClick={(event) => handleMenuClick(event)}>Recent Projects</span>
             <span className="menu-item" data-target="#employment" onClick={(event) => handleMenuClick(event)}>Employment History</span>
-            <span className="menu-item" data-target="#languages" onClick={(event) => handleMenuClick(event)}>Actual Languages</span>
             <span className="menu-item" data-target="#contact" onClick={(event) => handleMenuClick(event)}>Contact</span>
             <span className="menu-item" data-target="/short-fiction" onClick={(event) => handleMenuClick(event)}>Short Fiction</span>
         </Menu>
